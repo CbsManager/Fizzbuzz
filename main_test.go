79515956f32b3c3d441286b0ec40c4dbe691e248
Numan <numan.b@arise.tech>
2024-07-09 23:34:36 +0700

    redo case8

--- main_test.go
+++ main_test.go
@@ -72,4 +72,14 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("Expected %s but got %s", want, got)
 		}
 	})
+
+	t.Run("case 8 should return 8", func(t *testing.T) {
+		want := "8"
+
+		got := FizzBuzz(8)
+
+		if want != got {
+			t.Errorf("Expected %s but got %s", want, got)
+		}
+	})
 }
