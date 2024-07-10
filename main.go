62434d8907d6897c76b67cc63fbf1140a4a29f03
Numan <numan.b@arise.tech>
2024-07-10 19:52:27 +0700

    case 15

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func FizzBuzz(n int) string {
-	if n%3 == 0 {
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
 		return "Fizz"
 	} else if n%5 == 0 {
 		return "Buzz"
